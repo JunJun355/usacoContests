2de70223af607f1ceb08e4797fc088effda191a7
JunJun355 <jaywujun1@gmail.com>
2021-12-11 11:08:01 -0500

    Created more templates

---
+++ usaco/template.cpp
@@ -0,0 +1,19 @@
+/*
+ID: 06jajun
+TASK: test
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+
+    
+
+    return 0;
+}
