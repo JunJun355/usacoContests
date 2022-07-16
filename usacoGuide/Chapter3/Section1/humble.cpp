f1b59d107cb1ab56261cde908147fae2004fe648
JunJun355 <jaywujun1@gmail.com>
2022-07-16 12:10:52 -0400

    Create humble.cpp

---
+++ usacoGuide/Chapter3/Section1/humble.cpp
@@ -0,0 +1,24 @@
+/*
+ID: 06jajun
+TASK: humble
+LANG: C++17
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+#define MXN 1000
+#define INF 1000000000
+
+ifstream fin ("humble.in");
+ofstream fout ("humble.out");
+
+int main() {
+  
+}
