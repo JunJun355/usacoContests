35923f9bc2ff36634e00a508bdaddfbfd174c50b
JunJun355 <jaywujun1@gmail.com>
2022-07-18 17:11:47 -0400

    3.1.3

--- usacoGuide/Chapter3/Section1/humble.cpp
+++ usacoGuide/Chapter3/Section1/humble.cpp
@@ -8,17 +8,37 @@ LANG: C++17
 #include <algorithm>
 #include <vector>
 #include <fstream>
-#include <unordered_map>
-#include <string>
+#include <set>
 
 using namespace std;
 
-#define MXN 1000
-#define INF 1000000000
+#define MXK 100
+#define MXN 100000
+#define INF 1e18
 
+int k, n;
+int primes[MXK];
+long long curr, cap=INF;
+set<long long> humbles;
 ifstream fin ("humble.in");
 ofstream fout ("humble.out");
 
 int main() {
-  
+  fin >> k >> n;
+  for (int i=0; i<k; i++) {
+    fin >> primes[i];
+    humbles.insert(primes[i]);
+  }
+  sort(primes, primes + k);
+  for (int i=0; i<n; i++) {
+    curr = *humbles.begin();
+    humbles.erase(*humbles.begin());
+    if (humbles.size() > n - i) cap = *humbles.rbegin();
+    for (int j=0; j<k; j++) {
+      if (curr * primes[j] > cap) break;
+      humbles.insert(curr * primes[j]);
+      if (humbles.size() > MXN) humbles.erase(*humbles.rbegin());
+    }
+  }
+  fout << curr << endl;
 }
